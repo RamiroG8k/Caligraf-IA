030f2c6b67db667d6d930a9834b6d6e98247bfe1
Ramiro Méndez López <=>
2021-05-13 20:31:44 -0500

    carousel imgs try

--- screens/WelcomeScreen.tsx
+++ screens/WelcomeScreen.tsx
@@ -1,22 +1,49 @@
 import * as React from 'react';
-import { Image, StyleSheet } from 'react-native';
+import { FlatList, Image, StyleSheet } from 'react-native';
 import { Text, View, Button } from '../components/Themed';
 
-const WelcomeScreen = ({ navigation }: { navigation: any }, props: any) => {
+const arrayInfo: Array<Object> = [
+    {
+        title: 'Caligraf IA analysis Assessment.',
+        img: '../assets/images/illustrations/1.png',
+        description: 'Lorem ipsum dolor sit amet, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    },
+];
+
+const WelcomeScreen: React.FunctionComponent = (props: any) => {
+    const { navigation } = props;
+
+    const panel = (item: any): any => {
+        return (
+            <View transparent={true}>
+                <View transparent={true} style={styles.imgContainer}>
+                    <Image style={styles.img} source={{ uri: item.img }} />
+                </View>
+                <Text>{item.img}</Text>
+                <View transparent={true} style={{ height: '30%' }}>
+                    <Text secondary bold style={styles.title}>{item.title}</Text>
+                    <Text style={styles.subtitle}>{item.description}</Text>
+                </View>
+            </View>
+        );
+    }
+
     return (
         <View transparent={true} style={styles.container}>
-            <View transparent={true} style={styles.imgContainer}>
+            {/* <View transparent={true} style={styles.imgContainer}>
                 <Image style={styles.img}
                     source={require('../assets/images/illustrations/1.png')} />
             </View>
             <View transparent={true} style={{ height: '30%' }}>
                 <Text secondary bold style={styles.title}>Caligraf IA analysis Assessment.</Text>
                 <Text style={styles.subtitle}>Lorem ipsum dolor sit amet, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Text>
-            </View>
+            </View> */}
+
+            <FlatList data={arrayInfo} keyExtractor={(_, index) => index.toString()} renderItem={({item}) => panel(item)}/>
             <View transparent={true}>
                 <View transparent={true} style={styles.buttonGroup}>
-                    <Button text="Sign Up" onPress={() => navigation.navigate('Register')} style={{ width: '45%'}}/>
-                    <Button primary text="Log In" onPress={() => navigation.navigate('Login')} style={{ width: '45%'}}/>
+                    <Button text="Sign Up" onPress={() => navigation.navigate('Register')} style={{ width: '45%' }} />
+                    <Button primary text="Log In" onPress={() => navigation.navigate('Login')} style={{ width: '45%' }} />
                 </View>
             </View>
         </View>
@@ -37,6 +64,7 @@ const styles = StyleSheet.create({
     img: {
         flex: 1,
         width: '100%',
+        height: '100%',
     },
     title: {
         textAlign: 'center',
