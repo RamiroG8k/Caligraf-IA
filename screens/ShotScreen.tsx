4b304cc415c4321c5918e91328db14a84ec512fe
Ramiro Méndez López <=>
2021-06-15 23:22:19 -0500

    reamining to handle img

--- screens/ShotScreen.tsx
+++ screens/ShotScreen.tsx
@@ -1,3 +1,4 @@
+import { decode, encode } from 'base-64';
 // Common
 import React, { useEffect, useRef, useState } from 'react';
 import { StyleSheet } from 'react-native';
@@ -8,6 +9,8 @@ import { Ionicons } from '@expo/vector-icons';
 import * as ScreenOrientation from 'expo-screen-orientation';
 import { Camera } from 'expo-camera';
 import Layout from '../constants/Layout';
+import { ocrInstance } from '../services/instances';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const ShotScreen = (props: any) => {
     const { navigation, route } = props;
@@ -39,20 +42,20 @@ const ShotScreen = (props: any) => {
         );
     }
 
-    function dataURItoBlob(dataURI: string) {
+    function dataURItoBlob(dataURI: any) {
         // convert base64/URLEncoded data component to raw binary data held in a string
-        var byteString;
+        let byteString;
         if (dataURI.split(',')[0].indexOf('base64') >= 0)
-            byteString = atob(dataURI.split(',')[1]);
+            byteString = decode(dataURI.split(',')[1]);
         else
             byteString = unescape(dataURI.split(',')[1]);
 
         // separate out the mime component
-        var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+        let mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
 
         // write the bytes of the string to a typed array
-        var ia = new Uint8Array(byteString.length);
-        for (var i = 0; i < byteString.length; i++) {
+        let ia = new Uint8Array(byteString.length);
+        for (let i = 0; i < byteString.length; i++) {
             ia[i] = byteString.charCodeAt(i);
         }
 
@@ -60,31 +63,96 @@ const ShotScreen = (props: any) => {
     }
 
     const takeShot = async () => {
-        const photo = await ref.current.takePictureAsync({ quality: 0.8, base64: true });
-
-        async () => {
-            var blob = dataURItoBlob(photo.base64);
-            var IMG = new File([blob], 'imagen_try.jpg', { type: 'image/jpg' });
-    
-            console.log(typeof IMG);
-            console.log(IMG);
-        }
+        const photo = await ref.current
+            .takePictureAsync({
+                quality: 0.8,
+                base64: true
+            }).then((res: any) => {
+                // console.log(res);
+                let base64Img = `data:image/jpg;base64,${res.base64}`;
+                getFile(base64Img);
+                // getFile(res.base64);
+            }).catch((err: any) => {
+                console.log(err);
+            });
         // console.log(photo);
 
         // let image = new Image(photo.width, photo.height);
-        // let base64Img = `data:image/jpg;base64,${photo.base64}`;
         // image.src = base64Img;
-
-        
 
         // console.log(typeof image);
         // console.log(image);
 
-
         // await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT_UP);
         // navigation.goBack();
     }
-    
+
+    function b64toBlob(b64Data: string, contentType: string = 'image/jpg', sliceSize?: any) {
+        contentType = contentType || '';
+        sliceSize = sliceSize || 512;
+
+        var byteCharacters = decode(b64Data);
+        var byteArrays = [];
+
+        for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+            var slice = byteCharacters.slice(offset, offset + sliceSize);
+
+            var byteNumbers = new Array(slice.length);
+            for (var i = 0; i < slice.length; i++) {
+                byteNumbers[i] = slice.charCodeAt(i);
+            }
+
+            var byteArray = new Uint8Array(byteNumbers);
+
+            byteArrays.push(byteArray);
+        }
+
+        var blob = new Blob(byteArrays, { type: contentType });
+        return blob;
+    }
+
+    const getFile = async (base64: string) => {
+        let blob = dataURItoBlob(base64);
+        // let blob = b64toBlob(base64);
+        let IMG = new File([blob], 'imagen_try.jpg', { type: 'image/jpg' });
+
+        postFile();
+
+        async function postFile() {
+
+            let user: any;
+            AsyncStorage.getItem('info').then(
+                (response: any) => {
+                    if (response !== null) {
+                        user = (JSON.parse(response)._id);
+                        console.log(user);
+                        
+                        return;
+                    }
+                }
+            ).catch(
+                (error: any) => {
+                    console.warn('ERROR IN ASYNC STORAGE: ', error);
+                }
+            );
+
+
+            let formData = new FormData();
+            formData.append('image', IMG);
+            formData.append('phraseId', route.params.phrase._id);
+            // formData.append('userId', user);
+            // console.log(formData);
+            
+            await ocrInstance.post('/image', formData)
+                .then((response: any) => {
+                    console.log(response);
+                }).catch((error: any) => {
+                    console.warn(error);
+                });
+        }
+
+    }
+
     const goBack = async () => {
         await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT_UP);
         navigation.navigate('AnalizeScreen', { analized: false, backward: true });
